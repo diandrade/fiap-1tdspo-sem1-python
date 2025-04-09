39f6a3af37e120e5300d237adfd42306a27c67a6
diandrade <contato.andradediego@gmail.com>
2025-04-09 16:45:24 -0300

    Adição dos exercícios 1 e 3 da lista de While

--- lista_exercicios_for_e_while/while/exercicio_02.py
+++ lista_exercicios_for_e_while/while/exercicio_02.py
@@ -13,3 +13,38 @@ Calcule a média aritmética da turma e informe o resultado no final.
 
 E exiba na tela a quantidade de alunos: reprovados direto, exame aprovado.
 '''
+
+qtd_notas = 0
+media_aluno = 0
+media_final = 0
+aprovados = 0
+reprovados = 0
+exame = 0
+qtd_alunos = 0
+
+nota = float(input("Insira a nota do aluno entre 0 e 10: "))
+if nota < 0:
+    print("Insira um valor entre 0 e 10.")
+else:
+    while 0 <= nota <= 10:
+        nota += float(input("Insira a nota do aluno: "))
+        qtd_notas += 1
+
+        if qtd_notas == 4:
+            media_aluno = nota / qtd_notas
+            if media_aluno < 3.5:
+                reprovados += 1
+            elif media_aluno < 7:
+                exame += 1
+            else:
+                aprovados += 1
+
+        media_final += media_aluno
+        qtd_alunos += 1
+        qtd_notas = 0
+        nota = 0
+
+        media_final /= qtd_alunos
+        print(
+            f"A quantidade de aprovados foi de {aprovados}, de exame foram {exame} e de reprovados foram {reprovados}.")
+        print(f"A média final da turma foi de {media_final:.2f}")
--- lista_exercicios_for_e_while/while/exercicio_03.py
+++ lista_exercicios_for_e_while/while/exercicio_03.py
@@ -2,3 +2,20 @@
 Mostrar todos os inteiros entre dois números digitados pelo usuário.
 Exemplo: usuário digita os números 8 e 15, e aparecem em tela: 9, 10, 11, 12, 13, 14.
 '''
+
+num1 = int(input("Insira o primeiro valor do intervalo: "))
+num2 = int(input("Insira o segundo valor do intervalo: "))
+i = 0
+
+if num2 < num1:
+    intervalo = num1 - num2
+    menor = num2
+else:
+    intervalo = num2 - num1
+    menor = num1
+
+while i < intervalo-1:
+    menor += 1
+    i += 1
+
+    print(menor)
